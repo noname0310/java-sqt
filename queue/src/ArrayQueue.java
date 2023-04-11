4d9f34b476b60221e32a8b7b1161a0ff1324d005
noname <48761044+noname0310@users.noreply.github.com>
2023-04-11 10:05:38 +0900

    add queue base

---
+++ queue/src/ArrayQueue.java
@@ -0,0 +1,27 @@
+public class ArrayQueue<E> implements Queue<E> {
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void enqueue(E it) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E frontValue() {
+        return null;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+}
---
+++ queue/src/LinkedQueueTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedQueueTest {
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void enqueue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dequeue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void frontValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void length() {
+    }
+}
